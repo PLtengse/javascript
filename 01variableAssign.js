6f126e91a5b10f585979bb22b1ca043478a58a41
PLtengse <prasadtengse3@gmail.com>
2023-12-26 13:54:51 +0530

    addprofile
    addprofile

---
+++ 01variableAssign.js
@@ -0,0 +1,25 @@
+
+
+
+
+var myName = "Prasad Tengse";
+console.log( "myFullName",myName);
+var fullName ="Prasad Laxmanrao Tengse";
+var myLove = "I Love Only JS";
+console.log("myLove");
+
+var firstName ="Prasad";
+var lastName = "Tengse";
+var age = "26";
+var address ="Parbhani";
+var pinCode ="431401";
+var gender ="Male";
+var isMarried ="No"
+
+console.log("First Name: ",firstName );
+console.log("Last Name: ", lastName);
+console.log("Age: ", age);
+console.log("Address: ",address);
+console.log("Pincode: ",pinCode);
+console.log("Gender: ",gender);
+console.log("Married: ",isMarried);
